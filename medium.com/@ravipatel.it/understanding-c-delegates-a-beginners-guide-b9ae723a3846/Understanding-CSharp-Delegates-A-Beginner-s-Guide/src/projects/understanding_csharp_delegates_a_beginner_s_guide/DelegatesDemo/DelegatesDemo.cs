acfbf936b045d22450b5f2754304af23a9fee81f
Ramu Mohan Srinivasan Radhakrishnan <ramumsrk02071981@gmail.com>
2024-07-13 00:10:22 +0530

    Understanding C# Delegates A beginner's Guide
    https://medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846
    
    On branch main-Delegates-CSharp-Programming-Language-dotNET-Core
    
    Initial commit
    
    Changes to be committed:
    new file:   .gitignore
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/00-Introduction/00-Introduction.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/01-What-Are-Delegates/01-What-Are-Delegates.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/02-Why-Use-Delegates/02-Why-Use-Delegates.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/03-Defining-And-Using-Delegates/03-Defining-And-Using-Delegates.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/04-Real-World-Use-Case-Callback-Methods/04-Real-World-Use-Case-Callback-Methods.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/05-Tips-For-Beginners/05-Tips-For-Beginners.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/06-Conclusion/06-Conclusion.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/Understanding-CSharp-Delegates-A-Beginner-s-Guide.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/logs/dotnet_build_--verbosity_diagnostic.txt
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/logs/dotnet_new_create_console_--verbosity_diagnostic_--framework_net8.0_--language_csharp_--langVersion_12_--name_--output.txt
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/logs/dotnet_new_create_solution_--verbosity_diagnostic_--name_--output.txt
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/logs/dotnet_run_--verbosity_diagnostic_--project.txt
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/medium.com/@ravipatel.it/ravipatel.it.md
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/src/projects/understanding_csharp_delegates_a_beginner_s_guide/DelegatesDemo/DelegatesDemo.cs
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/src/projects/understanding_csharp_delegates_a_beginner_s_guide/DelegatesDemo/DelegatesDemo.csproj
    new file:   medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/src/solution/understanding_csharp_delegates_a_beginner_s_guide/understanding_csharp_delegates_a_beginner_s_guide.sln

---
+++ medium.com/@ravipatel.it/understanding-c-delegates-a-beginners-guide-b9ae723a3846/Understanding-CSharp-Delegates-A-Beginner-s-Guide/src/projects/understanding_csharp_delegates_a_beginner_s_guide/DelegatesDemo/DelegatesDemo.cs
@@ -0,0 +1,21 @@
+﻿var firstOperand = 10;
+var secondOperand = 20;
+char binaryOperator = '-';
+
+DelegateDemo.Computation addition = (int firstOperand, int secondOperand , char binaryOperator) => {
+  return binaryOperator switch {
+    '+' => firstOperand + secondOperand,
+    '-' => firstOperand - secondOperand,
+    '*' => firstOperand * secondOperand,
+    '/' => firstOperand / secondOperand,
+    '%' => firstOperand % secondOperand,
+    _ => 0
+  };
+};
+
+System.Console.WriteLine($"{Environment.NewLine}Applying the binary operator {binaryOperator} on operands {firstOperand} and {secondOperand} respectively results in {addition(firstOperand, secondOperand, binaryOperator)}");
+
+namespace DelegateDemo
+{
+  delegate int Computation(int firstOperand, int secondOperand, char binaryOperator);
+}
